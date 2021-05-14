edbb814a078a12ab3e94ba302648d1dbd8f54a34
brian32768 <brian@wildsong.biz>
2021-05-14 14:37:54 -0700

    Committing new release 2.20

--- setting/nls/strings.js
+++ setting/nls/strings.js
@@ -1,6 +1,6 @@
 /*global define*/
 ///////////////////////////////////////////////////////////////////////////
-// Copyright Â© Esri. All Rights Reserved.
+// Copyright © Esri. All Rights Reserved.
 //
 // Licensed under the Apache License Version 2.0 (the 'License');
 // you may not use this file except in compliance with the License.
